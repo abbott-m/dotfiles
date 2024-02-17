5f9eece761023c673fb22e1dce7912f0ff8b09c1
michael <michael@mabbott.dev>
2024-02-17 20:13:02 +0000

    theme changes

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -34,6 +34,7 @@ return {
       },
     },
   },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = {} },
-  { "nyoom-engineering/oxocarbon.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = {
+    transparent_background = true,
+  } },
 }
