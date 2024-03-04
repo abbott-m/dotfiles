587376faa07d8f2977d551ae7a78d2e4492be308
Michael Abbott <michael@mabbott.dev>
2024-03-04 14:50:39 +0000

    fix indentation for gofiles

---
+++ .config/nvim/lua/autocmd.lua
@@ -0,0 +1,20 @@
+local augroup = vim.api.nvim_create_augroup
+local opt = vim.opt
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'hcl', 'terraform' },
+  desc = 'terraform/hcl commentstring configuration',
+  command = 'setlocal commentstring=#\\ %s',
+})
+
+local indentation = augroup('identation', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = { '*.go' },
+  desc = 'correct tab / space size for *.go',
+  callback = function()
+    opt.tabstop = 4
+    opt.shiftwidth = 4
+    opt.softtabstop = 4
+  end,
+  group = indentation,
+})
--- .config/nvim/lua/base/plugins/colorscheme.lua
+++ .config/nvim/lua/base/plugins/colorscheme.lua
@@ -22,23 +22,23 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      local oxocarbon = require('oxocarbon').oxocarbon
-      vim.opt.background = 'dark'
-
-      -- vim.api.nvim_set_hl(0, 'Normal', { bg = oxocarbon.none })
-      -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = oxocarbon.none })
-
-      vim.api.nvim_set_hl(0, 'TelescopeBorder', { fg = oxocarbon.base03, bg = oxocarbon.none })
-      vim.api.nvim_set_hl(0, 'TelescopeMatching', { fg = oxocarbon.base14, bg = oxocarbon.none, bold = true, italic = true })
-      vim.api.nvim_set_hl(0, 'TelescopeNormal', { fg = oxocarbon.none, bg = oxocarbon.none })
-      vim.api.nvim_set_hl(0, 'TelescopePreviewLine', { fg = oxocarbon.none, bg = oxocarbon.base01 })
-      vim.api.nvim_set_hl(0, 'TelescopePreviewTitle', { fg = oxocarbon.base14, bg = oxocarbon.none })
-      vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { fg = oxocarbon.base03, bg = oxocarbon.none })
-      vim.api.nvim_set_hl(0, 'TelescopePromptNormal', { fg = oxocarbon.base04, bg = oxocarbon.none })
-      vim.api.nvim_set_hl(0, 'TelescopePromptPrefix', { fg = oxocarbon.base08, bg = oxocarbon.none })
-      vim.api.nvim_set_hl(0, 'TelescopePromptTitle', { fg = oxocarbon.base14, bg = oxocarbon.none })
-      vim.api.nvim_set_hl(0, 'TelescopeResultsTitle', { fg = oxocarbon.base14, bg = oxocarbon.none })
-      vim.api.nvim_set_hl(0, 'TelescopeSelection', { fg = oxocarbon.none, bg = oxocarbon.base02 })
+      -- local oxocarbon = require('oxocarbon').oxocarbon
+      -- vim.opt.background = 'dark'
+      --
+      -- -- vim.api.nvim_set_hl(0, 'Normal', { bg = oxocarbon.none })
+      -- -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = oxocarbon.none })
+      --
+      -- vim.api.nvim_set_hl(0, 'TelescopeBorder', { fg = oxocarbon.base03, bg = oxocarbon.none })
+      -- vim.api.nvim_set_hl(0, 'TelescopeMatching', { fg = oxocarbon.base14, bg = oxocarbon.none, bold = true, italic = true })
+      -- vim.api.nvim_set_hl(0, 'TelescopeNormal', { fg = oxocarbon.none, bg = oxocarbon.none })
+      -- vim.api.nvim_set_hl(0, 'TelescopePreviewLine', { fg = oxocarbon.none, bg = oxocarbon.base01 })
+      -- vim.api.nvim_set_hl(0, 'TelescopePreviewTitle', { fg = oxocarbon.base14, bg = oxocarbon.none })
+      -- vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { fg = oxocarbon.base03, bg = oxocarbon.none })
+      -- vim.api.nvim_set_hl(0, 'TelescopePromptNormal', { fg = oxocarbon.base04, bg = oxocarbon.none })
+      -- vim.api.nvim_set_hl(0, 'TelescopePromptPrefix', { fg = oxocarbon.base08, bg = oxocarbon.none })
+      -- vim.api.nvim_set_hl(0, 'TelescopePromptTitle', { fg = oxocarbon.base14, bg = oxocarbon.none })
+      -- vim.api.nvim_set_hl(0, 'TelescopeResultsTitle', { fg = oxocarbon.base14, bg = oxocarbon.none })
+      -- vim.api.nvim_set_hl(0, 'TelescopeSelection', { fg = oxocarbon.none, bg = oxocarbon.base02 })
     end,
   },
 }
