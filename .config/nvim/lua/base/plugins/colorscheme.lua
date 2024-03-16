0094ef416972c447378296a13c9d8a3d1f4cb538
Michael Abbott <michael@mabbott.dev>
2024-03-16 15:08:46 +0000

    colorscheme and plugins

--- .config/nvim/lua/base/plugins/colorscheme.lua
+++ .config/nvim/lua/base/plugins/colorscheme.lua
@@ -11,34 +11,24 @@ return {
   {
     'catppuccin/nvim',
     name = 'catppuccin',
+    opts = {
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = true,
+        mini = {
+          enabled = true,
+          indentscope_color = '',
+        },
+      },
+      transparent_background = true,
+    },
     priority = 1000,
     config = function()
       vim.cmd.hi 'Comment gui=none'
       vim.cmd.colorscheme 'catppuccin'
-    end,
-  },
-  {
-    'nyoom-engineering/oxocarbon.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- local oxocarbon = require('oxocarbon').oxocarbon
-      -- vim.opt.background = 'dark'
-      --
-      -- -- vim.api.nvim_set_hl(0, 'Normal', { bg = oxocarbon.none })
-      -- -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = oxocarbon.none })
-      --
-      -- vim.api.nvim_set_hl(0, 'TelescopeBorder', { fg = oxocarbon.base03, bg = oxocarbon.none })
-      -- vim.api.nvim_set_hl(0, 'TelescopeMatching', { fg = oxocarbon.base14, bg = oxocarbon.none, bold = true, italic = true })
-      -- vim.api.nvim_set_hl(0, 'TelescopeNormal', { fg = oxocarbon.none, bg = oxocarbon.none })
-      -- vim.api.nvim_set_hl(0, 'TelescopePreviewLine', { fg = oxocarbon.none, bg = oxocarbon.base01 })
-      -- vim.api.nvim_set_hl(0, 'TelescopePreviewTitle', { fg = oxocarbon.base14, bg = oxocarbon.none })
-      -- vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { fg = oxocarbon.base03, bg = oxocarbon.none })
-      -- vim.api.nvim_set_hl(0, 'TelescopePromptNormal', { fg = oxocarbon.base04, bg = oxocarbon.none })
-      -- vim.api.nvim_set_hl(0, 'TelescopePromptPrefix', { fg = oxocarbon.base08, bg = oxocarbon.none })
-      -- vim.api.nvim_set_hl(0, 'TelescopePromptTitle', { fg = oxocarbon.base14, bg = oxocarbon.none })
-      -- vim.api.nvim_set_hl(0, 'TelescopeResultsTitle', { fg = oxocarbon.base14, bg = oxocarbon.none })
-      -- vim.api.nvim_set_hl(0, 'TelescopeSelection', { fg = oxocarbon.none, bg = oxocarbon.base02 })
     end,
   },
 }
